40922042295a391043b0235f3b4eaa29425adc79
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-08-06 15:49:03 -0700

    Initial Commit

---
+++ TugOWar/TugOWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TugOWarApp.swift
+//  TugOWar
+//
+//  Created by Sean Kelley on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct TugOWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TugOWarTests/TugOWarTests.swift
@@ -0,0 +1,17 @@
+//
+//  TugOWarTests.swift
+//  TugOWarTests
+//
+//  Created by Sean Kelley on 2025-08-06.
+//
+
+import Testing
+@testable import TugOWar
+
+struct TugOWarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
